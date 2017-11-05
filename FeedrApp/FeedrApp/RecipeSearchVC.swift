9f6a59f0df57c3c4d5082118809b41d8fd1362d3
Chieh C "Jack" Chen <spdrcd@gmail.com>
2017-11-04 20:46:08 -0500

    added close keyboard in search view controller
    added search feature in view controller, set default launch screen

--- FeedrApp/FeedrApp/RecipeSearchVC.swift
+++ FeedrApp/FeedrApp/RecipeSearchVC.swift
@@ -67,7 +67,22 @@ class RecipeSearchVC: UITableViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
     }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
 
     override func didReceiveMemoryWarning()
     {
